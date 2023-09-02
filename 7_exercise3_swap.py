56c1307201fb730a7e826911f9bad2b2a2812a91
Shree <buchadeshree33@gmail.com>
2023-09-02 10:42:12 +0530

    swapping two numbers

---
+++ 7_exercise3_swap.py
@@ -0,0 +1,7 @@
+a = input("Enter a Number:")
+b = input("Enter a another Number:")
+print("Numbers before swap:\n a="+a+" b="+b)
+temp = a
+a = b
+b = temp
+print("Numbers after swap:\n a="+a+" b="+b)
