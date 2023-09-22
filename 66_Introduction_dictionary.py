a8bac02be5bea22bec1b24b81056a98f69c1205c
Shree <buchadeshree33@gmail.com>
2023-09-22 13:09:17 +0530

    Introduction to Dictionries

---
+++ 66_Introduction_dictionary.py
@@ -0,0 +1,19 @@
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.",
+  "Function": "A piece of code that you can easily call over and over again",
+  
+}
+# making empty dictionary of present dictionary
+# programming_dictionary={}
+
+# adding a item to a dictionary
+# programming_dictionary["Loops"]="The action of doing something over and over again."
+
+# editing item in the dictionary
+# programming_dictionary["Bug"]="A small problem"
+# print(programming_dictionary)
+
+# printing values in loop
+for a in programming_dictionary:
+  print(a)
+  print(programming_dictionary[a])
